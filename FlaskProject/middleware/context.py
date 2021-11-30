39c3e78e46d5e8af9468ad9abba2ed3a83dadde9
Praditya Raudi <pradityaraudi@gmail.com>
2021-11-29 23:22:30 -0500

    change the db config part

--- FlaskProject/middleware/context.py
+++ FlaskProject/middleware/context.py
@@ -1,6 +1,8 @@
 
 import os
 
+from .db_config import db_connect_info
+
 # This is a bad place for this import
 import pymysql
 
@@ -11,22 +13,21 @@ def get_db_info():
     :return: A dictionary with connect info for MySQL
     """
     db_host = os.environ.get("DBHOST", None)
+    db_user = os.environ.get("DBUSER", None)
+    db_password = os.environ.get("DBPASSWORD", None)
 
-    if db_host is None:
-
-        db_info = {
-            "host": "localhost",
-            "user": "dbuser",
-            "password": "dbuserdbuser",
-            "cursorclass": pymysql.cursors.DictCursor
-        }
-
-    else:
-
+    if db_host is not None:
         db_info = {
             "host": db_host,
-            "user": os.environ.get("DBUSER"),
-            "password": os.environ.get("DBPASSWORD"),
+            "user": db_user,
+            "password": db_password,
+            "cursorclass": pymysql.cursors.DictCursor
+        }
+    else:
+        db_info = {
+            "host": db_connect_info["DBHOST"],
+            "user": db_connect_info["DBUSER"],
+            "password": db_connect_info["DBPASSWORD"],
             "cursorclass": pymysql.cursors.DictCursor
         }
 
